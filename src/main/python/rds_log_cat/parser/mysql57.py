2886c8f3477928a4322faab45cf292e2f01fdb99
Michael Waldeck <michael.waldeck@scout24.com>
2017-03-31 11:29:26 +0200

    add mysql57 parser

--- src/main/python/rds_log_cat/parser/mysql57.py
+++ src/main/python/rds_log_cat/parser/mysql57.py
@@ -1,7 +1,35 @@
-from rds_log_cat.parser.parser import Parser
+from rds_log_cat.parser.parser import Parser, LineParserException
 
 
 class Mysql57(Parser):
 
     def __init__(self):
         Parser.__init__(self)
+
+    def compose_timestamp(self, datetime, timezone):
+        if not len(datetime) == 27:
+            raise LineParserException('wrong length of date')
+        if not timezone == 'UTC':
+            raise LineParserException('Only able to parse times in UTC. You gave {}'.format(timezone))
+        return datetime
+
+    def parse(self, line):
+        """
+        parses the fields in line to generate json structure
+        """
+        expected_min_no_fields = 5
+        result = {}
+        if len(line) < expected_min_no_fields:
+            raise LineParserException('line too short')
+
+        pid = line[1]
+        log_level = line[2].lstrip("[").rstrip("]")
+        timezone = 'UTC'
+
+        result = {
+            '@timestamp': self.compose_timestamp(line[0], timezone),
+            'log_level': log_level,
+            'process_id': int(pid),
+            'message': ' '.join(map(str, line[3:]))
+        }
+        return result
