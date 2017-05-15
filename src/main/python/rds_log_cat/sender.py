6b1f6bf35a1925253f532e7fefef4eb16149f3c3
Felix Borchers <felix.borchers@immobilienscout24.de>
2017-05-15 19:33:51 +0200

    add backoff on kinesis ProvisionedThroughputExceed

--- src/main/python/rds_log_cat/sender.py
+++ src/main/python/rds_log_cat/sender.py
@@ -30,16 +30,17 @@ def chunks(iterable, size):
 def send(chunk, stream_name, client=None):
     if not client:
         client = boto3.client('kinesis')
+    retries = 0
     while chunk:
         response = client.put_records(StreamName=stream_name, Records=chunk)
         failed_record_count = response['FailedRecordCount']
         if failed_record_count == 0:
-            logging.info("Successfully sent %i records", len(chunk))
+            logging.info("Successfully sent %i records.", len(chunk))
             return
 
-        logging.warn("%i out of %i records failed to be sent.",
+        logging.warn("%i out of %i records failed to be sent.\
+                     Retrying failed ...",
                      failed_record_count, len(chunk))
-        time.sleep(0.1)
 
         next_chunk = []
         for item, result in zip(chunk, response['Records']):
@@ -49,6 +50,11 @@ def send(chunk, stream_name, client=None):
                 else:
                     logging.warn("Unhandled ErrorCode: %r",
                                  result['ErrorCode'])
+                    logging.warn('Dropping this record.')
 
         chunk = next_chunk
-        logging.info("Gathered %i records for resending.", len(chunk))
+        retries += 1
+        wait_time = 0.1 * retries * retries
+        time.sleep(wait_time)
+        logging.info("Gathered %i records for resending.\
+         Retry: %i Wait: %r sec", len(chunk), retries, wait_time)
